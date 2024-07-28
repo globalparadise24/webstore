bf62f4f8b8ae8ac7e6ffae9f8da2ce7a4a62f04a
globalparadise24 <global.parardise.24@gmail.com>
2024-07-28 01:57:48 -0400

    Update rive-animation.js

--- rive-animation.js
+++ rive-animation.js
@@ -1,6 +1,7 @@
-"""const riveCanvas = document.getElementById("riveCanvas");
+const riveCanvas = document.getElementById("riveCanvas");
+
 const rive = new Rive({
     src: "https://cdn.rive.app/animations/vehicles.riv",
     canvas: riveCanvas,
     autoplay: true,
-});""",
+});
